bcf4076188e1edd8c5892d4739c838c8a3c65662
66011212038-lgtm <66011212038@msu.ac.th>
2025-10-05 20:46:38 +0700

    add some new code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,18 @@
 import { Routes } from '@angular/router';
-import { Order } from './components/order/order';
+//import { Order } from './components/order/order';
 //import { Detall } from './components/detall/detall';
 // import { Library } from './components/library/library';
  //import { ProfileUser } from './components/profile-user/profile-user';
  //import { ProfileAdmin } from './components/profile-admin/profile-admin';
 //import { Homepage } from './components/homepage/homepage';
-//import { EditProfile } from './components/edit-profile/edit-profile';
+import { EditProfile } from './components/edit-profile/edit-profile';
 
 export const routes: Routes = [
     //  {path: '', component: ProfileUser},
      //{path: 'profile_admin', component: ProfileAdmin},
        //{path: '', component: Homepage},
-    //    {path: 'edit_profile', component: EditProfile},
+        {path: '', component: EditProfile},
     //    {path: '', component: Library},
     //{path: '', component: Detall},
-    {path: '', component: Order},
+    // {path: '', component: Order},
 ];
